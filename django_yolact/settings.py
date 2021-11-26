629c391645d25f2be755705913f32779091f9e82
padhi-santosh <93069042+padhi-santosh@users.noreply.github.com>
2021-11-26 16:44:21 +0530

    Update settings.py

--- django_yolact/settings.py
+++ django_yolact/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6ld)q#fndz8d9%3hqsn3p8ni!-m6d^7$a0pt3s3yq!6nkanunb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
