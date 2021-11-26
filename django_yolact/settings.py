13c09300dda9ff40fb76a28626cdad0f9b1d85bd
padhi-santosh <93069042+padhi-santosh@users.noreply.github.com>
2021-11-26 15:59:54 +0530

    Update settings.py

--- django_yolact/settings.py
+++ django_yolact/settings.py
@@ -135,6 +135,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
